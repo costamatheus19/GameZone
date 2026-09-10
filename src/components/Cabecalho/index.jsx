a653bad32543ba6d75480d1eef73646224d2f715
costamatheus19 <costacaxiasmatheus@gmail.com>
2026-09-10 08:22:04 -0300

    parte de suporte sendo feita

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,19 +1,27 @@
 import styles from "./Cabecalho.module.css";
 import Search from "../SearchBar";
 import { Link } from "react-router-dom";
+
+
 function Cabecalho() {
   return (
-    <header className={styles.Cabecalho}>
+    <header className={styles.Cabecalho}> 
 
       <div>
-      <a href="/" className={styles.h1}>Game</a>
-      <a href="/" className={styles.a}>Hub</a>
+        <a href="/" className={styles.h1}>Game</a>
+        <a href="/" className={styles.a}>Hub</a>
       </div>
+
       <a href="/">Loja</a>
+
       <Link className={styles.favoritos} to="/favoritos">
         Favoritos
       </Link>
-      <Link to="/suporte">Suporte</Link>
+
+      <Link to="/suporte">
+        Suporte
+      </Link>
+
       <Search />
     </header>
   );
--- src/pages/Suporte.jsx
+++ src/pages/Suporte.jsx
@@ -2,6 +2,9 @@ import style from "./Suporte.module.css";
 import Cabecalho from "../components/Cabecalho/index";
 import { Link } from "react-router-dom";
 import Search from "../components/SearchBar";
+import { FaGear, FaWallet, FaDownLong, FaCirclePlay, FaPhone } from "react-icons/fa6";
+import { MdOutlineReportProblem, MdMarkEmailRead } from "react-icons/md";
+
 function Suporte() {
   return (
     <div className={style.suporte}>
@@ -26,15 +29,23 @@ function Suporte() {
           <div className={style.topicosSuporte}>
             <ul>
               <li>
+                <MdOutlineReportProblem fontSize={40} />
+                  <br />
                 <a>Problema de conta</a>
               </li>
               <li>
+                <FaDownLong  fontSize={40}/>
+                  <br />
                 <a>Dowloads e intalações</a>
               </li>
               <li>
+                <FaWallet fontSize={40}/>
+                  <br />
                 <a>Pagamentos e reembolsos</a>
               </li>
               <li>
+                <FaGear fontSize={40}/>
+                  <br />
                 <a>Problemas técnicos</a>
               </li>
             </ul>
@@ -60,20 +71,32 @@ function Suporte() {
             </div>
 
             <div className={style.contatos}>
-              <h2>Entre em Contato</h2>
-              <h2>
-                Entre em contato com nossa equipe de suporte através dos canais
+              <h2>Entre em Contato com nossa equipe de suporte através dos canais
                 abaixo:
               </h2>
               <p>
                 Não encontrou o que precisava?
                 <br /> Fale diretamente com nossa equipe!
               </p>
-              <h3>chat ao vivo</h3>
-                <h3>email</h3>
-                <h3>telefone</h3>
+              <h3><FaCirclePlay fontSize={20}/> chat ao vivo</h3>
+                <h3><MdMarkEmailRead fontSize={20}/> email</h3>
+                <h3><FaPhone fontSize={20}/> telefone</h3>
                 <p></p>
             </div>
+          </div>
+          <div className={style.SegurancaEajuda}>
+            <div className={style.dicasSeguranca}>
+              <h2>Dicas de segurança</h2>
+            <p>Fique atento a golpes e proteja sua conta seguindo<br />
+            nossas recomendações de segurança</p>
+            <button>Ver todas as Seguranças</button>
+            </div>
+            <div className={style.precisaDeAjuda}>
+              <h2>Precisa de ajuda?</h2>
+              <p>Nossa equipe está pronta para te ajudar com qualuquer problema.</p>
+              <button>Abrir chamado</button>
+            </div>
+
           </div>
         </div>
       </main>
